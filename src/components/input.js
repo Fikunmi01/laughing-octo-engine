9fd2dc62238be9e2c18a1e7090bdf1d8851d0109
Fikunmi01 <93198530+Fikunmi01@users.noreply.github.com>
2023-03-23 22:58:17 +0100

    update: fixed UI and media query

--- src/components/input.js
+++ src/components/input.js
@@ -1,74 +1,93 @@
 import "./input.css";
-import React, {useContext, useState, useEffect} from "react";
-import {Link, useNavigate} from "react-router-dom";
-import {ValueContext} from "../context/context";
+import React, { useContext, useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { ValueContext } from "../context/context";
 
 export const Input = () => {
-    let navigate = useNavigate()
-    // Here we initialize our value context && states
-    const {value, setValue} = useContext(ValueContext);
-    let [error, setErrors] = useState(false);
-    let [typing, setTyping] = useState(false);
-    let [cachedValue, setCachedValue] = useState(value);
+  let navigate = useNavigate();
+  // Here we initialize our value context && states
+  const { value, setValue } = useContext(ValueContext);
+  let [error, setErrors] = useState(false);
+  let [typing, setTyping] = useState(false);
+  let [cachedValue, setCachedValue] = useState(value);
 
+  //=======Styles
+  const disableButton = {
+    fontFamily: `'Kalam', cursive`,
+    cursor: "not-allowed",
+    title: "Please input some text to enable the button",
+  };
+  const activeButton = {
+    fontFamily: `'Kalam', cursive`,
+    cursor: "pointer",
+  };
+  //========
 
-    //=======Styles
-    const disableButton = {
-        fontFamily: `'Kalam', cursive`,
-        cursor: "not-allowed",
-        title: "Please input some text to enable the button",
-    };
-    const activeButton = {
-        fontFamily: `'Kalam', cursive`,
-        cursor: "pointer",
-    };
-    //========
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (value === "" || value === null) setErrors(true);
+    navigate("/output");
+    setValue("");
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        if (value === '' || value === null) setErrors(true);
-        navigate("/output");
-        setValue('')
-    };
+  const watchInputChange = (e) => {
+    setTyping(true);
+    setValue(e.target.value);
+  };
 
-    const watchInputChange = (e) => {
-        setTyping(true);
-        setValue(e.target.value);
+  //Watch the value and check to know when the field is empty then alert the  error
+  useEffect(() => {
+    if (typing) {
+      setErrors(value === "");
     }
+  }, [value]);
 
-    //Watch the value and check to know when the field is empty then alert the  error
-    useEffect(() => {
-        if (typing) {
-            setErrors(value === '');
-        }
-    }, [value])
+  return (
+    <div className="input-background">
+      <h2
+        style={{
+          position: "absolute",
+          top: `10%`,
+          left: `5%`,
+          fontFamily: `'Kalam', cursive`,
+          fontSize: `2rem`,
+        color: `#4caf59`
+        }}
+      >
+        Duplicate Character remover
+      </h2>
+      <div className="search-card">
+        <form
+          onSubmit={handleSubmit}
+          style={{ display: "flex", gap: "0.4rem", alignItems: "center" }}
+        >
+          <div className="input-span">
+            <input
+              type="text"
+              style={{
+                fontFamily: `'Kalam', cursive`,
+                borderColor: error && "red",
+              }}
+              value={value}
+              onChange={watchInputChange}
+              placeholder="Input your text"
+            />
+            {error && (
+              <p style={{ color: "red", fontSize: "1em", marginTop: "0.8rem" }}>
+                Sorry but you have to fill out the field
+              </p>
+            )}
+          </div>
 
-    return (
-        <div className="input-background">
-            <div className="search-card">
-                <form onSubmit={handleSubmit} style={{display: 'flex', gap: '0.4rem', alignItems: 'center'}}>
-                    <div className="input-span">
-                        <input
-                            type="text"
-                            style={{fontFamily: `'Kalam', cursive`, borderColor: error && 'red'}}
-                            value={value}
-                            onChange={watchInputChange}
-                            placeholder="Input your text"
-                        />
-                        {
-                            error && (
-                                <p style={{color: 'red', fontSize: '1em', marginTop: '0.8rem'}}>Sorry but you have to fill
-                                    out the field</p>)
-                        }
-
-                    </div>
-
-                    <button className="search-button" style={value.length === 0 ? disableButton : activeButton}
-                            type={'submit'}>
-                        Proceed
-                    </button>
-                </form>
-            </div>
-        </div>
-    );
+          <button
+            className="search-button"
+            style={value.length === 0 ? disableButton : activeButton}
+            type={"submit"}
+          >
+            Proceed
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
--- src/components/output.js
+++ src/components/output.js
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { ValueContext } from "../context/context";
 import CharacterList from "./iterate";
 import './output.css';
+import { Link } from "react-router-dom";
 
 export const Output = () => {
   // const chantValue = useContext(ValueContext);
@@ -10,6 +11,9 @@ export const Output = () => {
 
   return (
     <div className="output-background">
+      <Link to='/'>
+        <i className="uil uil-estate"></i>
+      </Link>
       <div className="output-card">
         <span className="output-value">
           <CharacterList savedVariable={value.value}/>
